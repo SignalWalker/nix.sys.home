96b424d9e33239a2e75e26c3bdc3f98ef868866c
Ash Walker <ash@ashwalker.net>
2023-07-15 14:31:25 -0400

    ...

--- flake.nix
+++ flake.nix
@@ -6,30 +6,6 @@
       url = "github:kamadorueda/alejandra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sysbase = {
-      url = "github:signalwalker/nix.sys.base";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.alejandra.follows = "alejandra";
-      inputs.homebase.follows = "homebase";
-      inputs.homelib.follows = "homelib";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    homelib = {
-      url = "github:signalwalker/nix.home.lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.alejandra.follows = "alejandra";
-      inputs.home-manager.follows = "home-manager";
-    };
-    homebase = {
-      url = "github:signalwalker/nix.home.base";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.alejandra.follows = "alejandra";
-      inputs.homelib.follows = "homelib";
-      inputs.home-manager.follows = "home-manager";
-    };
   };
   outputs = inputs @ {
     self,
@@ -38,47 +14,12 @@
   }:
     with builtins; let
       std = nixpkgs.lib;
-      hlib = inputs.homelib.lib;
-      home = hlib.home;
-      signal = hlib.signal;
-      sys = hlib.sys;
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-      signalModules.default = {
-        name = "sys.home.default";
-        dependencies = signal.flake.set.toDependencies {
-          flakes = inputs;
-          filter = [];
-          outputs = {
-            sysbase = {
-              nixosModules = ["default"];
-            };
-          };
-        };
-        outputs = dependencies: {
-          homeManagerModules = {
-            config,
-            lib,
-            ...
-          }: {
-          };
-          nixosModules = {
-            config,
-            lib,
-            ...
-          }: {
-            imports = [./nixos-module.nix];
-          };
-        };
-      };
-      homeConfigurations = home.configuration.fromFlake {
-        flake = self;
-        flakeName = "sys.home";
-        isNixOS = false;
-      };
-      nixosConfigurations = sys.configuration.fromFlake {
-        flake = self;
-        flakeName = "sys.home";
+      nixosModules.default = {...}: {
+        imports = [
+          ./nixos-module.nix
+        ];
       };
     };
 }
